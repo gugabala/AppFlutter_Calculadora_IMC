78511e2469ec66634b15a97fcd776e409ff5da6e
gugabala <gugabala@gmail.com>
2020-07-01 13:07:28 -0300

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,70 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurpleAccent,
-        actions: <Widget>[IconButton(icon: Icon(Icons.refresh), onPressed: () {  },)],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurpleAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                size: 120,
+                color: Colors.deepPurpleAccent,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Peso (Kg)",
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+              ),
+              Text(
+                "Info:",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+              ),
+            ],
+          ),
+        ));
   }
 }
