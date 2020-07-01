8583389e8eaab9b82f10c5aeb181c56ee6b7d7a5
gugabala <gugabala@gmail.com>
2020-07-01 14:05:40 -0300

    Projeto finalizado!

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus Dados!";
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetField() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus Dados!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levimento acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,62 +59,84 @@ class _HomeState extends State<Home> {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: () {},
+              onPressed: _resetField,
             )
           ],
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 120,
-                color: Colors.deepPurpleAccent,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Peso (Kg)",
-                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  size: 120,
+                  color: Colors.deepPurpleAccent,
                 ),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Altura (cm)",
-                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Peso (Kg)",
+                    labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                  ),
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+                  controller: weightController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira seu peso!";
+                    }
+                  },
                 ),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 10,
-                  bottom: 10,
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                  ),
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+                  controller: heightController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira sua altura!";
+                    }
+                  },
                 ),
-                child: Container(
-                  height: 50,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  child: Container(
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _calculate();
+                        }
+                      },
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      color: Colors.deepPurpleAccent,
                     ),
-                    color: Colors.deepPurpleAccent,
                   ),
                 ),
-              ),
-              Text(
-                "Info:",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
-              ),
-            ],
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(color: Colors.deepPurpleAccent, fontSize: 25),
+                ),
+              ],
+            ),
           ),
         ));
   }
